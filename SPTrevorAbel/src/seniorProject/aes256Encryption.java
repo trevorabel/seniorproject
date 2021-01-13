36ec4bf078c408a224bb65ec72880b7cac0bcda5
Trevor <Trevor@DESKTOP-VOV8F76>
2021-01-13 00:02:49 -0500

    added aes256Encryption class

---
+++ SPTrevorAbel/src/seniorProject/aes256Encryption.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package seniorProject;
+
+/**
+ *
+ * @author Trevor
+ */
+public class aes256Encryption {
+ 
+    //global varriables for entire class
+    String passedFileContents;    
+    //global varriables for encryption here
+    
+    //global varriables for decryption here
+    
+    //blank constructor here
+    public aes256Encryption()
+    {
+        
+    }
+    
+    /*method to pass through the string from the chosen text file for 
+      encryption and decryption of the file
+    */
+    public aes256Encryption(String str)
+    {
+        passedFileContents = str;
+    }
+    
+    //AES256 Encryption here
+    
+    //AES256 Decryption here
+    
+    //potentially make method to generate secret key/ salted IV for encryption
+}
